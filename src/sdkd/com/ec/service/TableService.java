fa591ab086d2b67e5ce5c70e3a91c2d0cef4d98f
zhaoshuai <1046936829@qq.com>
2016-07-07 12:23:38 +0800

    赵帅 更新了热卖商品和特价商品的servlet

--- src/sdkd/com/ec/service/TableService.java
+++ src/sdkd/com/ec/service/TableService.java
@@ -2,6 +2,9 @@ package sdkd.com.ec.service;
 
 import sdkd.com.ec.dao.BaseDao;
 import sdkd.com.ec.dao.impl.EbCommentDao;
+import sdkd.com.ec.dao.impl.EbNewsDao;
+import sdkd.com.ec.dao.impl.EbProductDao;
+import sdkd.com.ec.dao.impl.EbUserDao;
 import sdkd.com.ec.model.*;
 
 import java.util.ArrayList;
@@ -10,10 +13,31 @@ import java.util.List;
 /**
  * Created by zhaoshuai on 2016/7/7.
  */
-public class TableService extends BaseDao{
-        public List getTable(String tableName){
-            List<String> all = new ArrayList<String>();
-            all.add("akashd");
-            return null;
+public class TableService extends BaseDao {
+    public List<EbComment> getCommentTable() {
+        return new EbCommentDao().getComments();
+    }
+
+    public List<EbNews> getNewsTable() {
+        return new EbNewsDao().getNews();
+    }
+
+    public List<EbProduct> getProductTable() {
+        return new EbProductDao().getProducts();
+    }
+
+    public List<EbUser> getUserTable() {
+        return new EbUserDao().getEbUsers();
+    }
+
+    public List<EbProduct> getHotProductList(){
+        List<EbProduct> all = getProductTable();
+        List<EbProduct> res = new ArrayList<EbProduct>();
+        for(EbProduct x : all){
+               if(x.getEpBargain().compareTo("" + 1) == 0){
+                      res.add(x);
+               }
         }
+        return res;
+    }
 }
